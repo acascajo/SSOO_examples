16f6a17e7713ee3866f2b742652db7ab749ca050
Alberto <alcasgar@gmail.com>
2026-07-14 22:07:08 +0200

    8th example

---
+++ intro_c/8_example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void funcion(int a, int b); /* prototype */
+
+int main(int argc, char *argv[]) {
+  int x = 2;
+  int y = 5;
+
+  printf("Before x = %d, y = %d\n", x, y);
+
+  funcion(x, y);
+
+  printf("After x = %d, y = %d\n", x, y);
+}
+
+void funcion(int a, int b) {
+  a = 0;
+  b = 0;
+
+  printf("Inside function, a = %d, b = %d\n", a, b);
+  return;
+}
